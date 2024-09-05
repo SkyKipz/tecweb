8d761eb9a18d73de604f7f780893ae51ec0c7826
SkyKipz <aaron.moralesva@alumno.buap.mx>
2024-09-04 22:23:32 -0600

    Modificación con método isset()

--- sandbox/supervariables.php
+++ sandbox/supervariables.php
@@ -1,3 +1,7 @@
 <?php
-    echo $_GET['valor1'];
+    if (isset($_GET['valor1'])) {
+        echo "Sí se recibió y el valor es: " . $_GET['valor1'];
+    } else {
+        echo "No se envió ningún valor";
+    }
 ?>
