62b85f43bb737177104b0fef31380671504eb4e7
SkyKipz <aaron.moralesva@alumno.buap.mx>
2024-09-12 13:46:05 -0600

    Ejercicio 4 terminado

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -39,5 +39,11 @@
         
     ?>
     
+    <h2>Ejercicio 4</h2>
+    <p>Crear un arreglo cuyos índices van de 97 a 122 y cuyos valores son las letras de la ‘a’ a la ‘z’. Usa la función chr(n) que devuelve el caracter cuyo código ASCII es n para poner el valor en cada índice. </p>
+    <?php 
+        ASCII();
+    ?>
+
 </body>
 </html>
