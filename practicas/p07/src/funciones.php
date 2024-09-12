bc26bc59c6504a2932d4859aaa48b741e1f56b5b
SkyKipz <aaron.moralesva@alumno.buap.mx>
2024-09-11 23:01:15 -0600

    Ejercicio 2 agregado a funciones.php

--- practicas/p07/src/funciones.php
+++ practicas/p07/src/funciones.php
@@ -1,9 +1,40 @@
 <?php
-function esMultiploDe5y7($num) {
-    if ($num % 5 == 0 && $num % 7 == 0) {
-        return true;
-    } else {
-        return false;
+    function esMultiploDe5y7($num) {
+        if ($num % 5 == 0 && $num % 7 == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
-}
+
+    function generarSecuencia()
+    {
+        $secuencia = [];
+        $iteraciones = 0;
+        $cantidadNumeros = 0;
+        
+        while (true) {
+            $fila = [];
+            $fila[] = rand(1, 1000); 
+            $fila[] = rand(1, 1000); 
+            $fila[] = rand(1, 1000); 
+
+            $iteraciones++;
+            $cantidadNumeros += 3;
+            
+            if ($fila[0] % 2 != 0 && $fila[1] % 2 == 0 && $fila[2] % 2 != 0) {
+                $secuencia[] = $fila;
+                break;
+            }
+            
+            $secuencia[] = $fila;
+        }
+
+        echo "<h2>Secuencia obtenida:</h2>";
+        foreach ($secuencia as $fila) {
+            echo implode(", ", $fila) . "<br>";
+        }
+        echo "$cantidadNumeros números en $iteraciones iteraciones.";
+    }
+    
 ?>
