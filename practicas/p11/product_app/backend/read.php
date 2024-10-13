e9721adc4d8d786d0439044ac4424789ed122fbc
SkyKipz <aaron.moralesva@alumno.buap.mx>
2024-10-13 14:10:01 -0600

    Modificados para las busquedas correspondientes

--- practicas/p11/product_app/backend/read.php
+++ practicas/p11/product_app/backend/read.php
@@ -1,29 +1,28 @@
 <?php
-    include_once __DIR__.'/database.php';
+include_once __DIR__.'/database.php';
 
-    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
-    $data = array();
-    // SE VERIFICA HABER RECIBIDO EL ID
-    if( isset($_POST['id']) ) {
-        $id = $_POST['id'];
-        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-        if ( $result = $conexion->query("SELECT * FROM productos WHERE id = '{$id}'") ) {
-            // SE OBTIENEN LOS RESULTADOS
-			$row = $result->fetch_array(MYSQLI_ASSOC);
+// SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+$data = array();
 
-            if(!is_null($row)) {
-                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
-                foreach($row as $key => $value) {
-                    $data[$key] = utf8_encode($value);
-                }
-            }
-			$result->free();
-		} else {
-            die('Query Error: '.mysqli_error($conexion));
-        }
-		$conexion->close();
-    } 
+// SE VERIFICA HABER RECIBIDO EL TÉRMINO A BUSCAR
+if (isset($_POST['search'])) {
+    $searchTerm = $_POST['search'];
     
-    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
-    echo json_encode($data, JSON_PRETTY_PRINT);
+    // SE REALIZA LA QUERY DE BÚSQUEDA UTILIZANDO LIKE
+    $query = "SELECT * FROM productos WHERE nombre LIKE '%{$searchTerm}%' OR marca LIKE '%{$searchTerm}%' OR detalles LIKE '%{$searchTerm}%'";
+    
+    if ($result = $conexion->query($query)) {
+        // SE OBTIENEN LOS RESULTADOS
+        while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            $data[] = array_map('utf8_encode', $row); // Se codifican los datos a UTF-8
+        }
+        $result->free();
+    } else {
+        die('Query Error: ' . mysqli_error($conexion));
+    }
+    $conexion->close();
+} 
+
+// SE HACE LA CONVERSIÓN DE ARRAY A JSON
+echo json_encode($data, JSON_PRETTY_PRINT);
 ?>
